f998c64bb0fc6725590c632dda4fa38a8ef26cc1
miguelFV <seyko.21@hotmail.com>
2024-01-05 17:59:47 -0600

    Se crean scripts

---
+++ DDL/gestion/create_table_users.sql
@@ -0,0 +1,11 @@
+Use gestion;
+GO
+IF OBJECT_ID('dbo.USERS','U') IS NOT NULL
+    DROP TABLE dbo.USERS;
+GO
+
+CREATE TABLE dbo.USERS(
+    ID_USER identity not NULL
+    FIST_NAME varchar(40) not NULL
+    LAST_NAME varchar(40) not NULL
+);
